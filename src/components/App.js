7d44273afe9911d67eff2098b4e00dccc419b64a
Daniel Ufeli <danielufeli@yahoo.com>
2022-03-17 05:23:23 +0100

    Add NavBar

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,16 @@
 import { Route, Routes } from 'react-router-dom';
 import Lists from './Lists';
 import Details from './Details';
-import Header from './Header';
+import NavBar from './NavBar';
 
 const App = () => (
   <>
-    <div className="container">
-      <div className="panel-bg">
-        <Header />
-        <Routes>
-          <Route path="/" element={<Lists />} />
-          <Route path="/details" element={<Details />} />
-        </Routes>
-      </div>
+    <div>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Lists />} />
+        <Route path="/details" element={<Details />} />
+      </Routes>
     </div>
   </>
 );
---
+++ src/components/NavBar.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { FaArrowLeft, FaMicrophone, FaCog } from 'react-icons/fa';
+import { NavLink } from 'react-router-dom';
+
+const NavBar = () => (
+  <>
+    <nav className="navbar navbar-expand-lg navbar-dark primary-bg">
+      <div className="container-fluid">
+
+        <div>
+          <NavLink className="px-2 text-white" to="/"><FaArrowLeft /></NavLink>
+          <strong className="text-white">2022</strong>
+        </div>
+
+        <div>
+          <span className="text-white">Category</span>
+        </div>
+
+        <div className="d-flex">
+          <NavLink className="text-white me-3" to="#">
+            <FaMicrophone />
+          </NavLink>
+
+          <NavLink className="text-white" to="#">
+            <FaCog />
+          </NavLink>
+        </div>
+
+      </div>
+    </nav>
+  </>
+);
+
+export default NavBar;
